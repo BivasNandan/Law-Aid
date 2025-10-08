f7715fba7436b19289ffacfafef43e2536b87215
BivasNandan <bivasnandan2002@gmail.com>
2025-10-09 04:37:20 +0600

    added new functions for our users in the auth.controller.js

--- backend/utils/tokens.js
+++ backend/utils/tokens.js
@@ -1,16 +1,31 @@
 import jwt from "jsonwebtoken";
 
 export const generateTokenForRole = (role, res) => {
-    const token = jwt.sign({role}, process.env.JWT_SECRET, {
+    const roleToken = jwt.sign({role}, process.env.JWT_SECRET, {
         expiresIn: "7d",
     });
 
-    res.cookie("jwt", token,{
+    res.cookie("roleToken", roleToken,{
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true, //prevents XSS attacks
         sameSite: "strict", //CSRF attacks
         secure: process.env.NODE_ENV === "development" ? false : true
     });
 
-    return token;
+    return roleToken;
+};
+
+export const generateTokenForUserId = (userId, res) => {
+    const userToken = jwt.sign({userId}, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+    });
+
+    res.cookie("userToken", userToken,{
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true, //prevents XSS attacks
+        sameSite: "strict", //CSRF attacks
+        secure: process.env.NODE_ENV === "development" ? false : true
+    });
+
+    return userToken;
 };
